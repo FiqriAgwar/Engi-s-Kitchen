b970e52a5c7343c9495b969a2219973cd1c321e8
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2020-03-07 06:44:38 +0700

    Add money and time var

---
+++ Engi's Kitchen/Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    [HideInInspector]
+    public int inGameMoney = 0;
+    int maxGameMoney = 100;
+
+    float playTime = 60f;
+    public Image timerClock;
+    public Slider scoreBar;
+
+    void Update(){
+        timerClock.fillAmount += 1.0f/playTime * Time.deltaTime;
+        scoreBar.value = inGameMoney / maxGameMoney * 100f;
+    }
+
+    public void increaseMoney(int money){
+        inGameMoney += money;
+    }
+}
