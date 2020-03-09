1649c19c017eea0b287afb9f62434c6ae7fa759b
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2020-03-09 07:12:14 +0700

    TrashBin added

---
+++ Engi's Kitchen/Assets/Scripts/TrashBin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBin : MonoBehaviour
+{
+    private MouseCursor cursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(cursor == null){
+            cursor = GameObject.FindObjectOfType<MouseCursor>();
+        }
+    }
+
+    void OnMouseDown(){
+        cursor.currentCursorName = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
